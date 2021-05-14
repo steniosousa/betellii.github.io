7c51acebbc490ee13362936e562ce00714b7c2a3
Carlos Vinicius <viniciusdev.26@gmail.com>
2021-05-14 05:01:33 -0300

    adição do arquivo de scss do layout app

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+  .sass('resources/sass/app.scss', 'public/css')
+  .sass('resources/sass/layouts/app.scss', 'public/css/layouts');
 
 mix.js('resources/js/welcome.js', 'public/js')
     .sass('resources/sass/welcome.scss', 'public/css');
